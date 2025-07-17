cdaad5bbcd911ff5796796840069cbba99302778
Dmitriy Sedov <sedov.dmitriy19@pf5evzch.foreman-test.vm.prod-5.cloud.dataline>
2025-07-17 15:21:58 +0300

    добавил контйнер и три мкросервиса-проекта

--- src/single-spa/route-reuse-strategy.ts
+++ src/single-spa/route-reuse-strategy.ts
@@ -1,20 +1,24 @@
+// Исправленный вариант
 import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
 import { Injectable } from '@angular/core';
+
 @Injectable()
-export class MicroFrontendRouteReuseStrategy extends RouteReuseStrategy {
-    shouldDetach(): boolean {
-        // маршрут не сохраняется
-        return false;
-    }
-    store(): void { }
-    shouldAttach(): boolean {
-        return false;
-    }
-    // время присоединения маршрута
-    retrieve(): DetachedRouteHandle {
-        return null;
-    }
-    shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-        return future.routeConfig === curr.routeConfig || (future.data.app && (future.data.app === curr.data.app));
-    }
+export class MicroFrontendRouteReuseStrategy implements RouteReuseStrategy {
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+    return false;
+  }
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void { }
+  shouldAttach(route: ActivatedRouteSnapshot): boolean {
+    return false;
+  }
+  // Теперь возвращаем DetachedRouteHandle или null
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
+    return null;
+  }
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    return (
+      future.routeConfig === curr.routeConfig ||
+      (!!future.data.app && future.data.app === curr.data.app)
+    );
+  }
 }
